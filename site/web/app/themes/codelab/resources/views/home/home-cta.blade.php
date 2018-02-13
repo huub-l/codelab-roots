765ed84b18fa407dbef0fa4cbd03975db8cf1ebe
Webber Wang <webber@codelabstudios.com>
2018-02-12 17:44:40 -0800

    ""

--- site/web/app/themes/codelab/resources/views/home/home-cta.blade.php
+++ site/web/app/themes/codelab/resources/views/home/home-cta.blade.php
@@ -1,6 +1,6 @@
 <section class="Section Section--inverse Section-cta bg-dark">
    <div class="container text-center">
       <h1 class="py-3"> Want to work together? </h1>
-      <a href="/lastform1" class="Btn-outline btn btn-outline-primary btn-lg my-3"> Let's Talk</a>
+      <a href="/lastform/1" class="Btn-outline btn btn-outline-primary btn-lg my-3"> Let's Talk</a>
    </div>
 </section>
